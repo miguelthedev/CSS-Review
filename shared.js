069688489ee388b5df52c7d6592de2bfad599d64
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-29 12:22:03 -0700

    Made improvements on Javascript file.

--- shared.js
+++ shared.js
@@ -9,28 +9,35 @@ var mobileNav = document.querySelector('.mobile-nav');
 for(var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener('click', function(e) {
     e.preventDefault();
-    backdrop.style.display = 'block';
-    modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // modal.className = 'open' This overrides the class name
+    backdrop.classList.add('open');
+    modal.classList.add('open');
   });
 };
 
 backdrop.addEventListener('click', function() {
-  mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
-noButton.addEventListener('click', closeModal);
+if(noButton) {
+  noButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display =  'none';
+  // backdrop.style.display = 'none';
+  // modal.style.display =  'none';
+  backdrop.classList.remove('open');
+  if(modal) {
+    modal.classList.remove('open');
+  }
 }
 
 toggleButton.addEventListener('click', function(e) {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  backdrop.classList.add('open');
+  mobileNav.classList.add('open');
 });
-
-// mobileNav.addEventListener('click', function(e) {
-//   this.style.display = 'none';
-// });
